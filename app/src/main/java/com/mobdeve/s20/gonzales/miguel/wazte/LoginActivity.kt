cf6c414a21f14c194fedc685a906b5fba0e5308a
Miguel Gonzlaes <miguel_gonzales@dlsu.edu.ph>
2024-11-28 13:31:16 +0800

    Finished UI for login & signup, started on setting up Firebase for authentication

--- app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.mobdeve.s20.gonzales.miguel.wazte
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var etLoginUsername:EditText
+    lateinit var tvLoginUsername:TextView
+    lateinit var etLoginPassword:EditText
+    lateinit var tvLoginPasswordError:TextView
+    lateinit var btnLogin:Button
+    lateinit var tvSignup:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,26 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        /*
+        * Setup btnLogin behavior, on click, does the following:
+        * - Submits data in EditText to database, if there are errors, display errors in
+        * corresponding TextViews, if no errors, user is redirected to MainActivity
+        * */
+        btnLogin = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            // Move to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*
+        * Setup behavior of clickable signup link, on click start signup activity
+        * */
+        tvSignup = findViewById(R.id.tvSignup)
+        tvSignup.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.mobdeve.s20.gonzales.miguel.wazte
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnLogout:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        /*
+        * Set up btnLogout behavior, on click, destroys main activity, and brings user back to login
+        * screen.
+        * */
+        btnLogout = findViewById(R.id.btnLogout)
+        btnLogout.setOnClickListener{
+            finish()
         }
     }
 }
