5e4fa179c919206af6cca0e62e4c493f9329971a
enzzzoooo <lorenzo_ambrosio@dlsu.edu.ph>
2024-12-05 14:53:02 +0800

    Create Activities and Dialogs

--- app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s20/gonzales/miguel/wazte/MainActivity.kt
@@ -2,13 +2,19 @@ package com.mobdeve.s20.gonzales.miguel.wazte
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnLogout:Button
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var exitBtn: Button
+    lateinit var logoutBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val sideDrawerBtn: ImageButton = findViewById(R.id.sideDrawerBtn)
+
+        // Sidebar buttons
+        exitBtn = findViewById(R.id.exitBtn)
+
+        exitBtn.setOnClickListener{
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        // TODO: Logout btn
+
+        sideDrawerBtn.setOnClickListener{
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
         }
 
         /*
